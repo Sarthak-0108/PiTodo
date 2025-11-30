a4d2e3ed21af97894c4ca27bad7fabb47a0596c6
Sarthak-0108 <sarthakkumar0108@gmail.com>
2025-11-30 22:13:27 +0530

    Update deploy scripts and vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/PiTodo/",
+
   plugins: [react(), tailwindcss()],
   server: {
     host: true, // exposes to local network
